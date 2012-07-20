024e84068835f38ff2f85f0550110d8e4ec9f0d9
Miguel París Díaz <mparisdiaz@gmail.com>
2012-07-20 14:22:40 +0200

    Do not compile audio-tx.c.

--- jni/Android.mk
+++ jni/Android.mk
@@ -56,7 +56,6 @@ LOCAL_CPPFLAGS += -D__STDC_CONSTANT_MACROS
 LOCAL_MODULE := android-media
 LOCAL_SRC_FILES :=	$(MEDIA_SOURCES)/init-media.c $(MEDIA_SOURCES)/my-cmdutils.c $(MEDIA_SOURCES)/socket-manager.c \
 			$(MEDIA_SOURCES)/util/log.c $(MEDIA_SOURCES)/util/utils.c \
-			$(MEDIA_SOURCES)/tx/audio-tx.c \
 			$(MEDIA_SOURCES)/rx/sdp-manager.c $(MEDIA_SOURCES)/rx/video-rx.c $(MEDIA_SOURCES)/rx/audio-rx.c \
 			jni/media/init-log.c \
 			jni/media-oo/MediaTx.cpp \jni/media/rx/media-rx.c jni/media/media-port-manager.c \
