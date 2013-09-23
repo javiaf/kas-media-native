69ce4db9ad5abf612e35e0026b8d4c7088931821
Miguel París Díaz <mparisdiaz@gmail.com>
2013-09-23 19:50:20 +0200

    Add function to take port bound to a specific address

--- jni/media-oo/JNIMediaPortManager.cpp
+++ jni/media-oo/JNIMediaPortManager.cpp
@@ -29,6 +29,8 @@ static char* LOG_TAG = "NDK-socket-manager";
 extern "C" {
 	JNIEXPORT jlong JNICALL Java_com_kurento_kas_media_ports_MediaPortManager_takeMediaPortNative(
 				JNIEnv* env, jclass clazz, jint port);
+	JNIEXPORT jlong JNICALL Java_com_kurento_kas_media_ports_MediaPortManager_takeMediaAddressPortNative(
+				JNIEnv* env, jclass clazz, jstring address, jint port);
 	JNIEXPORT jint JNICALL Java_com_kurento_kas_media_ports_MediaPortManager_releaseMediaPortNative(
 				JNIEnv* env, jclass clazz, jlong mediaPortRef);
 }
@@ -52,6 +54,29 @@ Java_com_kurento_kas_media_ports_MediaPortManager_takeMediaPortNative(
 	return (long)mediaPort;
 }
 
+JNIEXPORT jlong JNICALL
+Java_com_kurento_kas_media_ports_MediaPortManager_takeMediaAddressPortNative(
+				JNIEnv* env, jclass clazz,
+				jstring address, jint port)
+{
+	const char *p_address = NULL;
+	MediaPort *mediaPort = NULL;
+
+	if (init_log() != 0)
+		media_log(MEDIA_LOG_WARN, LOG_TAG, "Couldn't init android log");
+
+	p_address = env->GetStringUTFChars(address, NULL);
+	if (p_address == NULL) {
+		media_log(MEDIA_LOG_ERROR, LOG_TAG, "OutOfMemoryError");
+		return -1;
+	}
+
+	mediaPort = MediaPortManager::takeMediaPort(p_address, port);
+	env->ReleaseStringUTFChars(address, p_address);
+
+	return (long)mediaPort;
+}
+
 JNIEXPORT jint JNICALL
 Java_com_kurento_kas_media_ports_MediaPortManager_releaseMediaPortNative(
 				JNIEnv* env, jclass clazz, jlong mediaPortRef)
@@ -68,3 +93,4 @@ Java_com_kurento_kas_media_ports_MediaPortManager_releaseMediaPortNative(
 
 	return ret;
 }
+
